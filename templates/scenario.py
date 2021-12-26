4ca2b1ea14e810bed5ab29b47c0ad4a4398462c6
Uladzimir Khasianevich <khasianevich.u.u@gmail.com>
2021-12-26 13:40:26 +0100

    Fixed a typo.

--- templates/scenario.py
+++ templates/scenario.py
@@ -2,7 +2,7 @@ from random import random
 
 class Scenario:
 
-    ndots = 100 # Overrides the value, defined by -P.
+    ndots = 100 # Overrides the value, defined by -D.
 
     def __init__(self, i): # Agrument i defines the number of the dot.
         self.input = dict(
